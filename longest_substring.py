9ab53f4e4303eb81efa203d3c277bcc86dc2a721
Rahul Patil <loginrahul90@gmail.com>
2017-10-07 23:33:33 +0200

    Update longest_substring.py

--- longest_substring.py
+++ longest_substring.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 
-def longest_substring(data=None):
+def longest_substring(s):
+    longest = 0
+    start = 0
+    visited = [ False for _ in range(256) ]
+    for i, char in enumerate(s):
+        if visited[ord(char)]:
+            while char != s[start]:
+                visited[ord(s[start])] = False
+                start += 1
+            start += 1
+        else:
+            visited[ord(char)] = True
+        longest = max(longest, i - start + 1  )
+    return longest
 
-    temp = ""
-    for i,s in enumerate(data):
-        temp += s
-        # print temp
-        for k,a in enumerate(data):
-            if i == k: continue
-            # print temp
-            if a not in temp:
-                temp += a
-            elif  len(temp) > len(data[k:]):
-                return temp
-            else: temp = a
-    return temp
-
-
-# print longest_substring("pwwkew")
+#print longest_substring("pwwkew")
 print longest_substring("abcabcbb")
 print longest_substring("au")
+print longest_substring("aab")
