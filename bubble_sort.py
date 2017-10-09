b6f316960eee58a2d4f0709d6ad0c05af92c1395
Rahul Patil <loginrahul90@gmail.com>
2017-10-09 10:15:31 +0200

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,15 +2,19 @@
 
 
 def bubble_sort(data):
-    for i,e in enumerate(data):
-        for j,k in enumerate(data):
-            if i == j: continue
-            #if data[j] == data[-1]: break
-            if e < k:
-                data[i],data[j]=data[j],data[i]
+
+    sorted = False
+
+    while not sorted:
+        sorted = True
+        for i in range(0,len(data)-1):
+            if data[i] > data[i+1]:
+                sorted = False
+                data[i+1],data[i] = data[i],data[i+1]
     return data
 
 
 print bubble_sort([3,4,1])
 print bubble_sort([5,9,1,0])
 print bubble_sort([0,9,6,8,5,4,3,2,1,7])
+print bubble_sort([1,2,3,5,4])
